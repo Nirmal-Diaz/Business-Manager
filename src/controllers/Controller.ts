738934db2997a4025913b1aeae796206df326cdc
Nirmal Diaz <nirmaldiaz@gmail.com>
2020-06-06 23:32:14 +0530

    Changed: Registry files are now JS objects rather than strings

--- src/controllers/Controller.ts
+++ src/controllers/Controller.ts
@@ -40,12 +40,12 @@ export class TableController {
 }
 
 export class RegistryController {
-    static async getFile(fileName: string) {
+    static async getParsedFile(fileName: string) {
         if (/^[a-zA-Z]{1,}[.]{1}json$/.test(fileName)) {
             //fileName is valid and is in the format "someFile.json"
             const fullRelativeFilePath = `./src/registries/${fileName}`;
             if (fs.existsSync(fullRelativeFilePath)) {
-                return fs.readFileSync(fullRelativeFilePath, "utf-8");
+                return JSON.parse(fs.readFileSync(fullRelativeFilePath, "utf-8"));
             } else {
                 throw { title: "Oops! Couldn't find that", titleDescription: "Ensure that you are requesting the right file", message: "Our file registry doesn't have the file you are requesting. Make sure that you provided the file name along with its extension", technicalMessage: "File doesn't exist in registry" };
             }
