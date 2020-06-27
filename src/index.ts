d0566b087d24449377085651b9d29c5c76a9a508
Nirmal Diaz <nirmaldiaz@gmail.com>
2020-06-27 16:29:41 +0530

    Added: WebSocket functionality to business manager

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,22 @@ import "reflect-metadata";
 
 import * as express from "express";
 import * as session from "express-session";
+import * as socketIo from "socket.io";
 
 import { musixRouter } from "./routers/MusixRouter";
 import { liveWallRouter } from "./routers/LiveWallRouter";
 import { mainRouter } from "./routers/MainRouter";
+
 /*
 =====================================================================================
-app: Setup
+app & io: Setup
 =====================================================================================
 */
 const port: number = 8080;
 const app = express();
-app.listen(port);
+const http = require('http').createServer(app);
+const io = socketIo(http);
+http.listen(port);
 /*
 =====================================================================================
 app: Middleware Setup
@@ -32,5 +36,16 @@ app: Router Setup
 app.use("/musix", musixRouter);
 app.use("/liveWall", liveWallRouter);
 app.use("/", mainRouter);
+/* 
+=====================================================================================
+io: Listeners Setup
+=====================================================================================
+*/
+io.on("connection", (socket) => {
+    socket.on("musix-broadcast", (params) => {
+        //NOTE: "musix-broadcast" event broadcasts a given event to all of its clients
+        socket.broadcast.emit(params.eventName, params);
+    });
+});
 
 console.log(`Express server status: Ready. Listening on port ${port}`);
