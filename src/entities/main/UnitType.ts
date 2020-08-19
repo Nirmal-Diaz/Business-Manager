cb3307a70072aba15ab03c9dd07ae589015b9929
Nirmal Diaz <nirmaldiaz@gmail.com>
2020-08-19 17:03:20 +0530

    Changed: QuantityType to UnitType

---
+++ src/entities/main/UnitType.ts
@@ -0,0 +1,21 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Material } from "./Material";
+import { Product } from "./Product";
+
+@Entity("unit_type", { schema: "business_manager" })
+export class UnitType {
+  @PrimaryGeneratedColumn({ type: "int", name: "id" })
+  id: number;
+
+  @Column("varchar", { name: "name", length: 45 })
+  name: string;
+
+  @Column("varchar", { name: "symbol", length: 20 })
+  symbol: string;
+
+  @OneToMany(() => Material, (material) => material.unitType)
+  materials: Material[];
+
+  @OneToMany(() => Product, (product) => product.quantityType)
+  products: Product[];
+}
