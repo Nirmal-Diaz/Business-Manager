97f435ecd4ec0ed763e4fae433e32242f09adddb
Nirmal Diaz <nirmaldiaz@gmail.com>
2020-12-27 12:54:52 +0530

    Removed: Unnecessary script files

--- etc/pwaExtractor.sh
+++
@@ -1,6 +0,0 @@
-rm -r "/home/assassino/Cloud/Archives/Web/Progressive Web Apps/LiveWall + Musix/public";
-rm -r "/home/assassino/Cloud/Archives/Web/Progressive Web Apps/LiveWall + Musix/src";
-cp -r "../public" "../src" "/home/assassino/Cloud/Archives/Web/Progressive Web Apps/LiveWall + Musix";
-cd "/home/assassino/Cloud/Archives/Web/Progressive Web Apps/LiveWall + Musix";
-rm -r "./src/controllers" "./src/entities" "./src/repositories" "./src/registries/main" "./src/routers/main";
-rm -r "./public/images/main" "./public/layouts/main" "./public/scripts/main" "./public/stylesheets/main";
