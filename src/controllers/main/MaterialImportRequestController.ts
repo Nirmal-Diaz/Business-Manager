f385523e59ae18e92b0c2e1212c9fe02c859c45a
Nirmal Diaz <nirmaldiaz@gmail.com>
2021-01-10 17:53:39 +0530

    Changed: The way of writing the entity name

--- src/controllers/main/MaterialImportRequestController.ts
+++ src/controllers/main/MaterialImportRequestController.ts
@@ -6,7 +6,7 @@ import { MaterialImportRequest as Entity } from "../../entities/main/MaterialImp
 import { MaterialImportRequestRepository as EntityRepository } from "../../repositories/main/MaterialImportRequestRepository";
 
 export class MaterialImportRequestController {
-    private static entityName: string = "material import request";
+    private static entityName: string = "Material Import Request";
     private static entityJSONName: string = "materialImportRequest";
 
     static async createMany(clientBindingObject, selectedSupplierIds) {
@@ -45,7 +45,7 @@ export class MaterialImportRequestController {
         if (item) {
             return item;
         } else {
-            throw { title: "Oops!", titleDescription: "Please recheck your arguments", message: `We couldn't find a ${this.entityName} that matches your arguments`, technicalMessage: `No ${this.entityName} for given arguments` };
+            throw { title: "Oops!", titleDescription: "Please recheck your arguments", message: `We couldn't find a ${this.entityName.toLowerCase()} that matches your arguments`, technicalMessage: `No ${this.entityName.toLowerCase()} for given arguments` };
         }
     }
 
@@ -55,7 +55,7 @@ export class MaterialImportRequestController {
         if (items.length > 0) {
             return items;
         } else {
-            throw { title: "Couldn't find anything", titleDescription: "Try single words instead of phrases", message: `There are no ${this.entityName}s matching the keyword you provided`, technicalMessage: `No ${this.entityName}s for given keyword` };
+            throw { title: "Couldn't find anything", titleDescription: "Try single words instead of phrases", message: `There are no ${this.entityName.toLowerCase()}s matching the keyword you provided`, technicalMessage: `No ${this.entityName.toLowerCase()}s for given keyword` };
         }
     }
 
@@ -69,14 +69,14 @@ export class MaterialImportRequestController {
         if (items.length > 0) {
             return items;
         } else {
-            throw { title: "No Import Requests", titleDescription: "Try another request status", message: `There are no ${this.entityName}s for the status you specified`, technicalMessage: `No ${this.entityName}s with given status` };
+            throw { title: `No ${this.entityName}`, titleDescription: "Try another status", message: `There are no ${this.entityName.toLowerCase()}s for the status you specified`, technicalMessage: `No ${this.entityName.toLowerCase()}s with given status` };
         }
     }
 
     static async deleteOne(id: number) {
         return getRepository(Entity).delete(id).catch((error) => {
             //WARNING: This error is thrown based on a possible error and not on the actual error
-            throw { title: "First things first", titleDescription: "Remove any dependant records", message: `There are dependant records for this ${this.entityName}. Please remove those before removing this ${this.entityName}`, technicalMessage: error.sqlMessage }
+            throw { title: "First things first", titleDescription: "Remove any dependant records", message: `There are dependant records for this ${this.entityName.toLowerCase()}. Please remove those before removing this ${this.entityName.toLowerCase()}`, technicalMessage: error.sqlMessage }
         });
     }
 }
