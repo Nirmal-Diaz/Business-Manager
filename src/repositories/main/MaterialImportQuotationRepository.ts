7b96796fdc28c6171239fc90f9ef7e1ac038ad27
Nirmal Diaz <nirmaldiaz@gmail.com>
2021-01-10 18:31:53 +0530

    Changed: Searching tables

--- src/repositories/main/MaterialImportQuotationRepository.ts
+++ src/repositories/main/MaterialImportQuotationRepository.ts
@@ -6,7 +6,9 @@ export class MaterialImportQuotationRepository {
     static search(keyword) {
         return getRepository(MaterialImportQuotation)
         .createQueryBuilder("miq")
+        .leftJoinAndSelect("miq.unitType", "ut")
         .where("miq.code LIKE :keyword", { keyword: `%${keyword}%` })
+        .where("ut.name LIKE :keyword", { keyword: `%${keyword}%` })
         .getMany();
     }
 
@@ -16,6 +18,7 @@ export class MaterialImportQuotationRepository {
             UPDATE material_import_quotation miq
             SET miq.quotation_status_id =
             CASE
+                WHEN 4 THEN 4
                 WHEN DATEDIFF(miq.valid_from, NOW()) > 0 THEN 1
                 WHEN DATEDIFF(miq.valid_till, NOW()) > 0 THEN 2
                 ELSE 3
