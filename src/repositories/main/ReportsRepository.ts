a281ba6bc987ca692957ceac3afac6bfb74be3ab
Nirmal Diaz <nirmaldiaz@gmail.com>
2021-01-28 19:23:28 +0530

    Fixed: NULL fields are returned insted of 0

--- src/repositories/main/ReportsRepository.ts
+++ src/repositories/main/ReportsRepository.ts
@@ -5,24 +5,28 @@ export class ReportsRepository {
     static getSalesReportByYear(startDate: string, endDate: string) {
         return getRepository(ProductExportInvoice)
         .query(`
-            SELECT Year(pei.added_date) group_id, COUNT(pei.id) sales_count, SUM(pei.final_price) expected_income, SUM(ip.price) actual_income, SUM(pei.final_price)-SUM(ip.price) debts
+            SELECT *, expected_income-actual_income debts
+            FROM
+            (SELECT YEAR(pei.added_date) group_id, COUNT(pei.id) sales_count, IFNULL(SUM(pei.final_price),0) expected_income, IFNULL(SUM(ip.price),0) actual_income
             FROM product_export_invoice pei
             LEFT JOIN inbound_payment ip
             ON pei.code = ip.invoice_code
             WHERE pei.added_date BETWEEN "${startDate}" AND "${endDate}"
-            GROUP BY Year(pei.added_date)
+            GROUP BY group_id) sales_table
         `);
     }
 
     static getSalesReportByMonthOrWeek(startDate: string, endDate: string, groupBy: string) {
         return getRepository(ProductExportInvoice)
         .query(`
-            SELECT CONCAT(Year(pei.added_date), "-", ${groupBy}(pei.added_date)) group_id, COUNT(pei.id) sales_count, SUM(pei.final_price) expected_income, SUM(ip.price) actual_income, SUM(pei.final_price)-SUM(ip.price) debts
+            SELECT *, expected_income-actual_income debts
+            FROM
+            (SELECT CONCAT(YEAR(pei.added_date), "-", ${groupBy}(pei.added_date)) group_id, COUNT(pei.id) sales_count, IFNULL(SUM(pei.final_price),0) expected_income, IFNULL(SUM(ip.price),0) actual_income
             FROM product_export_invoice pei
             LEFT JOIN inbound_payment ip
             ON pei.code = ip.invoice_code
             WHERE pei.added_date BETWEEN "${startDate}" AND "${endDate}"
-            GROUP BY group_id
+            GROUP BY group_id) sales_table
         `);
     }
 
@@ -30,7 +34,9 @@ export class ReportsRepository {
         //NOTE: Since MySQL doesn't have an explicit Full Outer Join we have to use union on the results of left join and the right join
         return getRepository(ProductExportInvoice)
         .query(`
-            SELECT IFNULL(income_table.group_id, expenditure_table.group_id) group_id, expenditure_table.expenditure, income_table.income, IFNULL(income_table.income, 0) - IFNULL(expenditure_table.expenditure, 0) revenue FROM
+            SELECT *, income-expenditure revenue FROM
+
+            (SELECT IFNULL(income_table.group_id, expenditure_table.group_id) group_id, IFNULL(expenditure,0) expenditure, IFNULL(income,0) income FROM
             (SELECT YEAR(pei.added_date) group_id, SUM(pei.price) income
             FROM product_export_invoice pei
             WHERE pei.added_date BETWEEN "${startDate}" AND "${endDate}"
@@ -44,7 +50,7 @@ export class ReportsRepository {
             
             UNION
             
-            SELECT IFNULL(income_table.group_id, expenditure_table.group_id) group_id, expenditure_table.expenditure, income_table.income, IFNULL(income_table.income, 0) - IFNULL(expenditure_table.expenditure, 0) revenue FROM
+            SELECT IFNULL(income_table.group_id, expenditure_table.group_id) group_id, IFNULL(expenditure,0) expenditure, IFNULL(income,0) income FROM
             (SELECT YEAR(pei.added_date) group_id, SUM(pei.price) income
             FROM product_export_invoice pei
             WHERE pei.added_date BETWEEN "${startDate}" AND "${endDate}"
@@ -54,7 +60,7 @@ export class ReportsRepository {
             FROM material_import_invoice mii
             WHERE mii.added_date BETWEEN "${startDate}" AND "${endDate}"
             GROUP BY group_id) expenditure_table
-            ON income_table.group_id = expenditure_table.group_id
+            ON income_table.group_id = expenditure_table.group_id) income_expenditure_table
         `);
     }
 
@@ -62,7 +68,9 @@ export class ReportsRepository {
         //NOTE: Since MySQL doesn't have an explicit Full Outer Join we have to use union on the results of left join and the right join
         return getRepository(ProductExportInvoice)
         .query(`
-            SELECT IFNULL(income_table.group_id, expenditure_table.group_id) group_id, expenditure_table.expenditure, income_table.income, IFNULL(income_table.income, 0) - IFNULL(expenditure_table.expenditure, 0) revenue FROM
+            SELECT *, income-expenditure revenue FROM
+
+            (SELECT IFNULL(income_table.group_id, expenditure_table.group_id) group_id, IFNULL(expenditure,0) expenditure, IFNULL(income,0) income FROM
             (SELECT CONCAT(YEAR(pei.added_date), "-", ${groupBy}(pei.added_date)) group_id, SUM(pei.price) income
             FROM product_export_invoice pei
             WHERE pei.added_date BETWEEN "${startDate}" AND "${endDate}"
@@ -76,7 +84,7 @@ export class ReportsRepository {
             
             UNION
             
-            SELECT IFNULL(income_table.group_id, expenditure_table.group_id) group_id, expenditure_table.expenditure, income_table.income, IFNULL(income_table.income, 0) - IFNULL(expenditure_table.expenditure, 0) revenue FROM
+            SELECT IFNULL(income_table.group_id, expenditure_table.group_id) group_id, IFNULL(expenditure,0) expenditure, IFNULL(income,0) income FROM
             (SELECT CONCAT(YEAR(pei.added_date), "-", ${groupBy}(pei.added_date)) group_id, SUM(pei.price) income
             FROM product_export_invoice pei
             WHERE pei.added_date BETWEEN "${startDate}" AND "${endDate}"
@@ -86,7 +94,7 @@ export class ReportsRepository {
             FROM material_import_invoice mii
             WHERE mii.added_date BETWEEN "${startDate}" AND "${endDate}"
             GROUP BY group_id) expenditure_table
-            ON income_table.group_id = expenditure_table.group_id
+            ON income_table.group_id = expenditure_table.group_id) income_expenditure_table
         `);
     }
 }
