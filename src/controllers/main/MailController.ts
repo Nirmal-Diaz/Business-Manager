cb4d3b18127cc5a9d92f3fb1ce1b43eaa83ef0db
Nirmal Diaz <nirmaldiaz@gmail.com>
2021-04-20 10:48:48 +0530

    Changed: Mail format

--- src/controllers/main/MailController.ts
+++ src/controllers/main/MailController.ts
@@ -12,95 +12,77 @@ export class MailController {
     static async sendTemporalPassword(email, temporalPassword) {
         const mailOptions = {
             from: 'official.cyberspacetechnologies@gmail.com',
-            to: email,
+            to: "nirmaldiaz@gmail.com",
             subject: 'Temporal authentication code for Thames Coating International',
             html: `
-            <p>Thames Coating International Pvt. Ltd.</p>
-            <p>Weliveriya</p>
-            <p>Gampaha</p>
-        
-            </br></br>
+            <p>Thames Coating International Pvt. Ltd, </br>Weliveriya, </br>Gampaha</p>
             
             <h3>Here is your temporal authentication code</h3>
-            <p>Dear employee,</p><p>Please type the following temporal authentication code in the prompt to login to your user account.</p><p>Please note that this code can be used only once and within a period of 15 minutes.</p><p>We strongly recommend you to change your pattern after you log in.</p>
-            
-            </br></br>
+
+            <p>Dear employee, </br>Please type the following temporal authentication code in the prompt to login to your user account. Please note that this code can be used only once and within a period of 15 minutes. We strongly recommend you to change your pattern after you log in.</p>
 
             <h3>${temporalPassword}</h3>
-
-            </br></br>
                 
-            <p>Yours sincerely,</p>
-            <p>Systems administrator</p>`
+            <p>Yours sincerely,</br>Systems administrator</p>`
         };
 
         MailController.transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
-                throw { title: "Couldn't mail", titleDescription: "Contact your system administrator", message: `We couldn't send the temporal password to ${email}. But don't worry, your system administrator can reset the password for you.`, technicalMessage: `Couldn't send temporal password` };
-            } else {
-                return true;
+                throw { title: "Couldn't mail", titleDescription: "Contact your system administrator", message: `We couldn't send the temporal password to ${email}. But don't worry, your system administrator can reset the password for you.`, technicalMessage: "Couldn't send mail with temporal password" };
             }
+
+            return true;
         });
     }
 
     static async sendCustomerGreeting(email, personName, businessName) {
         const mailOptions = {
             from: 'official.cyberspacetechnologies@gmail.com',
-            to: email,
+            to: "nirmaldiaz@gmail.com",
             subject: 'You have been registered at Thames Coating International',
             html: `
-            <p>Thames Coating International Pvt. Ltd.</p>
-            <p>Weliveriya</p>
-            <p>Gampaha</p>
-        
-            </br></br>
+            <p>Thames Coating International Pvt. Ltd, </br>Weliveriya, </br>Gampaha</p>
             
             <h3>You have been registered as a customer</h3>
-            <p>Hello ${personName} of ${businessName}</p><p>This is to notify that your company has been successfully registered as a customer in our company.</p><p>Thank you for choosing us and we hope that you will choose us again.</p>
+
+            <p>Hello ${personName} of ${businessName}, </br>This is to notify that your company has been successfully registered as a customer in our company. Thank you for choosing us and we hope that you will choose us again.</p>
             
             </br></br>
                 
-            <p>Yours sincerely,</p>
-            <p>Manger of external relationships</p>`
+            <p>Yours sincerely, </br>Manger of external relationships</p>`
         };
 
         MailController.transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
-                throw { title: "Couldn't mail", titleDescription: "We'll try again later", message: `We couldn't send the greeting email to ${email}`, technicalMessage: `Couldn't send greetings email` };
-            } else {
-                return true;
+                throw { title: "Couldn't mail", titleDescription: "We'll try again later", message: `We couldn't send the customer greetings email to ${email}`, technicalMessage: "Couldn't send mail with customer greetings" };
             }
         });
+
+        return true;
     }
 
     static async sendSupplierGreeting(email, personName, businessName) {
         const mailOptions = {
             from: 'official.cyberspacetechnologies@gmail.com',
-            to: email,
+            to: "nirmaldiaz@gmail.com",
             subject: 'You have been registered at Thames Coating International',
             html: `
-            <p>Thames Coating International Pvt. Ltd.</p>
-            <p>Weliveriya</p>
-            <p>Gampaha</p>
-        
-            </br></br>
+            <p>Thames Coating International Pvt. Ltd, </br>Weliveriya, </br>Gampaha</p>
             
             <h3>You have been registered as a supplier</h3>
-            <p>Hello ${personName} of ${businessName}</p><p>This is to notify that your company has been successfully registered as a supplier in our company.</p><p>We are happy to have you as a supplier and we are looking forward to initiate business with you.</p>
-            
-            </br></br>
+
+            <p>Hello ${personName} of ${businessName},</br>This is to notify that your company has been successfully registered as a supplier in our company. We are happy to have you as a supplier and we are looking forward to initiate business with you.</p>
                 
-            <p>Yours sincerely,</p>
-            <p>Manger of external relationships</p>`
+            <p>Yours sincerely, </br>Manger of external relationships</p>`
         };
 
         MailController.transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
-                throw { title: "Couldn't mail", titleDescription: "We'll try again later", message: `We couldn't send the greeting email to ${email}`, technicalMessage: `Couldn't send greetings email` };
-            } else {
-                return true;
+                console.log({ title: "Couldn't mail", titleDescription: "We'll try again later", message: `We couldn't send the supplier greetings email to ${email}`, technicalMessage: "Couldn't send mail with supplier greetings"});
             }
         });
+
+        return true;
     }
 
     static async sendMaterialImportRequest(email, materialImportRequest) {
@@ -109,23 +91,13 @@ export class MailController {
             to: "nirmaldiaz@gmail.com",
             subject: 'Quotation request from Thames Coating International',
             html: `
-            <p>Thames Coating International Pvt. Ltd.</p>
-            <p>Weliveriya</p>
-            <p>Gampaha</p>
+            <p>Thames Coating International Pvt. Ltd, </br>Weliveriya, </br>Gampaha</p>
         
-            </br></br>
-        
-            <p>${materialImportRequest.supplier.personName}</p>
-            <p>Exports Manager</p>
-            <p>${materialImportRequest.supplier.businessName}</p>
-            <p>${materialImportRequest.supplier.address}</p>
-        
-            </br></br>
+            <p>${materialImportRequest.supplier.personName}</br>Exports Manager</br>${materialImportRequest.supplier.businessName}</br>${materialImportRequest.supplier.address}</p>
             
             <h3>Your quotation is needed</h3>
+
             <p>We kindly request you to send us your quotation to be considered in our next material purchase. Please refer the following table.</p>
-        
-            </br></br>
         
             <table border="1">
             <tbody>
@@ -140,19 +112,16 @@ export class MailController {
                 </tr>
             </tbody>
             </table>
-            
-            </br></br>
                 
-            <p>Yours sincerely,</p>
-            <p>Manager of imports and exports</p>`
+            <p>Yours sincerely, </br>Manager of imports and exports</p>`
         };
 
         MailController.transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
-                throw { title: "Couldn't mail", titleDescription: "We'll try again later", message: `We couldn't send the greeting email to ${email}`, technicalMessage: `Couldn't send greetings email` };
-            } else {
-                return true;
+                console.log({ title: "Couldn't mail", titleDescription: "We'll try again later", message: `We couldn't send material import request email to ${email}`, technicalMessage: "Couldn't send mail with material import request" });
             }
         });
+
+        return true;
     }
 }
