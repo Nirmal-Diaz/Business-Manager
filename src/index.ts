377ee9066bcf7962d2edc3e8484e43d10cd3b952
Nirmal Diaz <nirmaldiaz@gmail.com>
2021-05-05 08:12:53 +0530

    Removed: Unwanted files and routers

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,7 @@ import "reflect-metadata";
 import * as http from "http";
 
 import * as express from "express";
-import * as socketIo from "socket.io";
 
-import { musixRouter } from "./routers/MusixRouter";
-import { liveWallRouter } from "./routers/LiveWallRouter";
 import { mainRouter } from "./routers/MainRouter";
 
 /*
@@ -17,26 +14,12 @@ app & io: Setup
 const port: number = 8080;
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
 server.listen(port);
 /*
 =====================================================================================
 app: Router Setup
 =====================================================================================
 */
-app.use("/musix", musixRouter);
-app.use("/liveWall", liveWallRouter);
 app.use("/", mainRouter);
-/* 
-=====================================================================================
-io: Listeners Setup
-=====================================================================================
-*/
-io.of("/musix").on("connection", (socket) => {
-    socket.on("broadcast-event", (params) => {
-        //NOTE: "broadcast-event" event broadcasts a given event to all of its clients
-        socket.broadcast.emit(params.eventName, params);
-    });
-});
 
 console.log(`Express server status: Ready. Listening on port ${port}`);
